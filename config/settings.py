efdb65aae7b544d2364e6189cdd8969961d84883
Camilo Toro <camilotoro@email.edu.co>
2026-05-07 13:45:48 -0500

    Redireccionando el enlace de Django al Propio Login

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_URL = 'login'
+LOGIN_URL = 'proyectos_academicos:login'
 LOGIN_REDIRECT_URL = 'proyectos_academicos:lista_proyectos'
 LOGOUT_REDIRECT_URL = 'proyectos_academicos:login'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
